57f4ad6e6552fb4c4447d6973916588cd2deaf58
张盛 <zhangs@aifusheng.com>
2022-01-12 11:47:58 +0800

    add

--- api/workflow.go
+++ api/workflow.go
@@ -1,10 +1,8 @@
 package api
 
 import (
-	"encoding/json"
 	"errors"
 	"log"
-	"net/url"
 
 	"github.com/go-resty/resty/v2"
 )
@@ -22,24 +20,13 @@ func (a *API) Copytemplate(openTemplateId string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	qs := make(url.Values)
-	qs.Add("access_token", token)
-	apiUrl := copytemplateURI + "?" + qs.Encode()
-	data, err := json.Marshal(map[string]string{"open_template_id": openTemplateId})
-	if err != nil {
-		return "", err
-	}
-	body, err := a.Client.PostJSON(apiUrl, data)
-	log.Println(string(body))
-	if err != nil {
-		return "", err
-	}
 	result := &struct {
-		Errcode    int    `json:"errcode"`
-		Errmsg     string `json:"errmsg"`
+		BaseResp   `json:",inline"`
 		TemplateId string `json:"template_id"`
 	}{}
-	err = json.Unmarshal(body, result)
+	_, err = resty.New().R().SetResult(result).SetQueryParam("access_token", token).SetBody(map[string]string{
+		"open_template_id": openTemplateId,
+	}).Post(copytemplateURI)
 	if err != nil {
 		return "", err
 	}
@@ -49,16 +36,56 @@ func (a *API) Copytemplate(openTemplateId string) (string, error) {
 	return result.TemplateId, err
 }
 
-func (a *API) GetTemplateDetail(templateId string) (map[string]interface{}, error) {
+// 获取模板详情
+func (a *API) GetTemplateDetail(templateId string) (*TemplateDetailObj, error) {
 	token, err := a.Tokener.Token()
 	if err != nil {
 		return nil, err
 	}
-	var result map[string]interface{}
+	var result TemplateDetailObj
 	resp, err := resty.New().R().
 		SetQueryParam("access_token", token).
 		SetBody(map[string]string{"template_id": templateId}).
 		SetResult(&result).Post(gettemplatedetailURI)
 	log.Println(resp.String(), err)
-	return result, err
+	return &result, err
+}
+
+type TemplateDetailObj struct {
+	BaseResp      `json:",inline"`
+	TemplateNames []struct {
+		Text string `json:"text"`
+	} `json:"template_names"`
+	TemplateContent TemplateContent `json:"template_content"`
+}
+
+type TemplateContent struct {
+	Controls []struct {
+		Property Property `json:"property"`
+	} `json:"controls"`
+}
+
+type Property struct {
+	Control string `json:"control"`
+	ID      string `json:"id"`
+	Title   []struct {
+		Text string `json:"text"`
+	} `json:"title"`
+	Placeholder []struct {
+		Text string `json:"text"`
+	} `json:"placeholder"`
+	Require int64 `json:"require"`
+	UnPrint int64 `json:"un_print"`
+}
+
+// 提交申请
+func (a *API) Applyevent(body map[string]interface{}) (*map[string]interface{}, error) {
+	token, err := a.Tokener.Token()
+	if err != nil {
+		return nil, err
+	}
+	var result map[string]interface{}
+	resp, err := resty.New().R().SetQueryParam("access_token", token).SetBody(body).SetResult(&result).Post(applyeventURI)
+	log.Println(resp.String(), err)
+	return &result, err
 }
