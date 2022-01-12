5bac99bf0b11ce2712da961d4c4a68ba6a613f99
张盛 <zhangs@aifusheng.com>
2022-01-12 13:43:05 +0800

    add

--- api/workflow.go
+++ api/workflow.go
@@ -15,6 +15,73 @@ const (
 	getapprovaldetailURI = "https://qyapi.weixin.qq.com/cgi-bin/oa/getapprovaldetail" // 获取审批申请详情
 )
 
+type ApplyObj struct {
+	CreatorUserid       string        `json:"creator_userid"`
+	TemplateID          string        `json:"template_id"`
+	UseTemplateApprover int64         `json:"use_template_approver"`
+	Approver            []Approver    `json:"approver"`
+	Notifyer            []string      `json:"notifyer"`
+	NotifyType          int64         `json:"notify_type"`
+	ApplyData           ApplyData     `json:"apply_data"`
+	SummaryList         []SummaryList `json:"summary_list"`
+}
+
+type ApplyData struct {
+	Contents []Content `json:"contents"`
+}
+
+type Content struct {
+	Control string        `json:"control"`
+	ID      string        `json:"id"`
+	Title   []SummaryInfo `json:"title"`
+	Value   Value         `json:"value"`
+}
+
+type SummaryInfo struct {
+	Text string `json:"text"`
+	Lang string `json:"lang"`
+}
+
+type Value struct {
+	Text string `json:"text"`
+}
+
+type Approver struct {
+	Attr   int64    `json:"attr"`
+	Userid []string `json:"userid"`
+}
+
+type SummaryList struct {
+	SummaryInfo []SummaryInfo `json:"summary_info"`
+}
+
+type TemplateDetailObj struct {
+	BaseResp      `json:",inline"`
+	TemplateNames []struct {
+		Text string `json:"text"`
+	} `json:"template_names"`
+	TemplateContent TemplateContent `json:"template_content"`
+}
+
+type TemplateContent struct {
+	Controls []struct {
+		Property Property `json:"property"`
+	} `json:"controls"`
+}
+
+type Property struct {
+	Control string `json:"control"`
+	ID      string `json:"id"`
+	Title   []struct {
+		Text string `json:"text"`
+	} `json:"title"`
+	Placeholder []struct {
+		Text string `json:"text"`
+	} `json:"placeholder"`
+	Require int64 `json:"require"`
+	UnPrint int64 `json:"un_print"`
+}
+
 func (a *API) Copytemplate(openTemplateId string) (string, error) {
 	token, err := a.Tokener.Token()
 	if err != nil {
@@ -51,35 +118,8 @@ func (a *API) GetTemplateDetail(templateId string) (*TemplateDetailObj, error) {
 	return &result, err
 }
 
-type TemplateDetailObj struct {
-	BaseResp      `json:",inline"`
-	TemplateNames []struct {
-		Text string `json:"text"`
-	} `json:"template_names"`
-	TemplateContent TemplateContent `json:"template_content"`
-}
-
-type TemplateContent struct {
-	Controls []struct {
-		Property Property `json:"property"`
-	} `json:"controls"`
-}
-
-type Property struct {
-	Control string `json:"control"`
-	ID      string `json:"id"`
-	Title   []struct {
-		Text string `json:"text"`
-	} `json:"title"`
-	Placeholder []struct {
-		Text string `json:"text"`
-	} `json:"placeholder"`
-	Require int64 `json:"require"`
-	UnPrint int64 `json:"un_print"`
-}
-
 // 提交申请
-func (a *API) Applyevent(body map[string]interface{}) (*map[string]interface{}, error) {
+func (a *API) Applyevent(body ApplyObj) (*map[string]interface{}, error) {
 	token, err := a.Tokener.Token()
 	if err != nil {
 		return nil, err
