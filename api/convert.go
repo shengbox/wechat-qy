f7e18e11343734bdbd567a03f3c848a8e659d32d
zhangsheng <shengbox@gmail.com>
2026-06-13 17:17:43 +0800

    新增 FromServiceExternalUserID 方法，支持将第三方/代开发应用获取的 external_userid 转换成自建应用的 external_userid

--- api/convert.go
+++ api/convert.go
@@ -36,3 +36,38 @@ func (a *API) GetNewExternalUserid(externalUseridList []string) (*NewExternalUse
 	return result, err
 }
 
+const fromServiceExternalUserIDURI = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/from_service_external_userid"
+
+// FromServiceExternalUserIDRes Represents the response for converting service external userid to selfbuilt external userid
+type FromServiceExternalUserIDRes struct {
+	BaseResp `json:",inline"`
+	ExternalUserID string `json:"external_userid"`
+}
+
+// FromServiceExternalUserID converts external_userid from third-party/DK app to self-built app
+func (a *API) FromServiceExternalUserID(externalUserid string, sourceAgentID int) (*FromServiceExternalUserIDRes, error) {
+	token, err := a.Tokener.Token()
+	if err != nil {
+		return nil, err
+	}
+	qs := make(url.Values)
+	qs.Add("access_token", token)
+	apiUrl := fromServiceExternalUserIDURI + "?" + qs.Encode()
+	data, err := json.Marshal(map[string]any{
+		"external_userid": externalUserid,
+		"source_agentid":  sourceAgentID,
+	})
+	if err != nil {
+		return nil, err
+	}
+	body, err := a.Client.PostJSON(apiUrl, data)
+	if err != nil {
+		return nil, err
+	}
+	base.GetLogger().Println(string(body))
+	result := &FromServiceExternalUserIDRes{}
+	err = json.Unmarshal(body, result)
+	return result, err
+}
+
+
